79e6ccba7c07af6d89d3a5a8c0fc7856570fb786
Michael Korem <mkor82@gmail.com>
2021-09-26 00:53:19 -0400

    commit

--- Achievement 2.py
+++ Achievement 2.py
@@ -1,1 +1,9 @@
-print ("Please insert a a capital or low key letter ")
+x = 0
+x = str(input("Please enter a capital or low key letter "))
+
+if str.isalpha (x):
+    print ("A letter was entered")
+
+
+else:
+    print ("Incorrect value was entered")
