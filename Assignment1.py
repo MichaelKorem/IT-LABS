7ed472cb159575ac181fd69f9fce9ed075a25ead
Michael Korem <mkor82@gmail.com>
2021-09-26 22:00:05 -0400

    init

--- Assignment1.py
+++ Assignment1.py
@@ -1,13 +1,3 @@
-x = 0
-x = str(input("Please enter one capital or low key letter "))
-len (x)
-1
+color = 0
+color = int(input("What colour of shirt would you like to order"))
 
-if x.isupper ():
-    print ("Your letter was changed to lower case: " + x.lower ())
-
-elif x.islower ():
-    print ("Your letter was changed to upper case: " + x.upper ())
-
-else:
-    print ("Incorrect value was entered")
