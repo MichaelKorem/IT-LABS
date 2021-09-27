d060a2f0fe86e8c1ca76e8e5fd5a7a6075078425
Michael Korem <mkor82@gmail.com>
2021-09-26 22:45:02 -0400

    First Condition Done

--- Assignment1.py
+++ Assignment1.py
@@ -6,32 +6,36 @@ print ("""Welcome to Abby’s Merchandizing!
 What colour of shirt would you like to order?
 1. White 2. Black 3. Red 4. Blue 5. Green 6. Yellow""")
 
-color = int(input("Please pick the number adjacent to the preferred color: "))
+color = str(input("Please pick the number adjacent to the preferred color: "))
 
-if color =  1:
-    str(color) = White
+if color ==  "1":
+    color = "White"
     print ("Thank you. The chosen color is " + color)
 
-elif color =  2:
-    str(color) = Black
+elif color ==  "2":
+    color = "Black"
     print ("Thank you. The chosen color is " + color)
 
-elif color =  3:
-    str(color) = Red
+elif color ==  "3":
+    color = "Red"
     print ("Thank you. The chosen color is " + color)
 
-elif color =  4:
-    str(color) = Blue
+elif color == "4":
+    color = "Blue"
     print ("Thank you. The chosen color is " + color)   
 
-elif color =  5:
-    str(color) = Green
+elif color ==  "5":
+    color = "Green"
     print ("Thank you. The chosen color is " + color)
 
-elif color =  6:
-    str(color) = Yellow
+elif color ==  "6":
+    color = "Yellow"
     print ("Thank you. The chosen color is " + color)
 
 else:
     print ("Incorrect value was entered")
+    exit ()
+
+
+
 
