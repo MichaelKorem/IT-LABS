e0066f5788d5c024bbad98af9f665d768cf29dbb
Michael Korem <mkor82@gmail.com>
2021-10-25 10:37:36 -0400

    graphix done

---
+++ Final Project.py
@@ -0,0 +1,59 @@
+vocabulary = ["absurd", "awkward", "bandwagon", "bookworm", "espionage", "funny", "galaxy", "gazebo", "gossip", "injury", "jazz", "jigsaw", "jukebox", "keyhole", "length", "lucky", "luxury", "matrix", "oxygen", "pixel", "puzzling", "quiz", "rhubarb", "strength", "stronghold", "transcript", "unknown", "wizard", "wristwatch", "zipper"]
+
+m1 = ("""
+
+
+
+
+
+^^^^^^^^""")
+m2 = ("""
+|
+|
+|
+|
+|
+^^^^^^^^""")
+m3 = ("""_______
+|
+|
+|
+|
+|
+^^^^^^^^""")
+m4 = ("""_______
+|     |
+|     o
+|
+|
+|
+^^^^^^^^""")
+m5 = ("""_______
+|     |
+|     o
+|     |
+|
+|
+^^^^^^^^""")
+m6 = ("""_______
+|     |
+|     o
+|    /|\ 
+|
+|
+^^^^^^^^""")
+m7 = ("""_______
+|     |
+|     o
+|    /|\ 
+|    / \ 
+|
+^^^^^^^^""")
+
+print(m1)
+print(m2)
+print(m3)
+print(m4)
+print(m5)
+print(m6)
+print(m7)
