d95b63bf47a1d54084bbc62d959ec4074bafcf51
Michael Korem <mkor82@gmail.com>
2021-10-28 10:43:33 -0400

    init

---
+++ Achievement 5.py
@@ -0,0 +1,17 @@
+source = "Lorem ipsum doflor szit ametß, consqecytetu$r adipibsching elit, sed do eiusmod tewmporα incid6kgidunt ut labq8ore et café dolore_ magna aliqua. Purus! sit{|}~ \t\n\r\x0b\x0c amet volutpat cons&jequat mau7ris. MichaelKorem8757102Penaxtibus et"
+
+class Characters:
+    def CountName(self):
+        print()
+
+    def CountNum(self):
+        print()
+
+    def CountAlNum(self):
+        print()
+
+    def CountNonAlNum(self):
+        print()
+
+    def CountSpace(self):
+        print()
