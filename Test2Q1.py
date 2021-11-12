431f5693de235e4195dde761d6b3cace494cef90
Michael Korem <mkor82@gmail.com>
2021-11-12 08:57:26 -0500

    Final

--- Test2Q1.py
+++ Test2Q1.py
@@ -1,3 +1,16 @@
 class Calculator:
-    def __init__(self, input):
+    def factorial(sum,num):
+        sum = num
+        while num-1 > 0:
+            sum = sum*(num-1)
+            num = num -1
+        print(sum)
         
+while True:
+    try:            
+        n = int(input("Please insert a number: "))
+    except ValueError:
+        print ("Incorrect value was entered. Please try again.")
+        continue
+    Calculator.factorial(sum,n)
+    break 
