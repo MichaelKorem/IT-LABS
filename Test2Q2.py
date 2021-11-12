3e626dd4d92180dac56aaee71b4df3cd7a3967f4
Michael Korem <mkor82@gmail.com>
2021-11-12 09:14:22 -0500

    Update

--- Test2Q2.py
+++ Test2Q2.py
@@ -1,2 +1,24 @@
 class LargestValue:
+    def __init__(self, name):
+        self.name = name
+        self.numbers = []
     
+    def maxValue (self):
+        while True:
+            try:
+                n1 = float(input("Enter first number: "))
+                n2 = float(input("Enter second number: "))
+                n3 = float(input("Enter third number: "))
+
+                if (n1 >= n2) and (n1 >= n3):
+                    largest = n1
+                elif (n2 >= n1) and (n2 >= n3):
+                    largest = n2
+                else:
+                    largest = n3
+
+                print("The largest number is", largest)
+            
+            except ValueError:
+                print ("Incorrect value was entered. Please try again.")
+                continue
