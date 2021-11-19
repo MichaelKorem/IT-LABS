9c946e6d3a0607de8fd4d19fea9533f555f88c59
Michael Korem <mkor82@gmail.com>
2021-11-19 16:13:31 -0500

    Interface Done

--- Final Project.py
+++ Final Project.py
@@ -48,4 +48,26 @@ Gallow = ["""
 def RandomWord(WordList):
    WordNum = random.randint(0, len(WordList)-1)
    return WordList[WordNum]
-print(RandomWord(WordList))
+
+def Interface(WrongGuess, CorrectGuess, ChosenWord):
+    if len(WrongGuess) > 0:
+        print(Gallow[len(WrongGuess)-1])
+        print("""
+Wrong guesses:""")
+        for Letter in WrongGuess:
+            print(Letter, end=" ")
+    if len(WrongGuess) >= 0 and len(WrongGuess) < len(ChosenWord):       
+        print("""
+
+The secret word is:
+""")
+
+        HiddenWord = "_" * len(ChosenWord)
+
+        for i in range(len(ChosenWord)):
+            if ChosenWord[i] in CorrectGuess:
+                HiddenWord = HiddenWord[:i] + ChosenWord[i] + HiddenWord[i+1:]
+
+        for Letter in HiddenWord:
+            print(Letter, end=" ")
+        print()
