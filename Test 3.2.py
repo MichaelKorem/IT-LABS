e12b4d81baadecdeacd1f6b37fe18c15e573714a
Michael Korem <mkor82@gmail.com>
2021-12-03 10:03:40 -0500

    Finished

--- Test 3.2.py
+++ Test 3.2.py
@@ -1,29 +1,44 @@
-class Rectangle
+class Rectangle:
 
-def __init__(self):
-    self.perimeter = 0
-    self.area = 0
+    def __init__(self):
+        self.length = 0
+        self.width = 0
+        self.perimeter = 0
+        self.area = 0
 
-def getPerimeter(self):
-    length = float(input("Please insert the length of the rectangle:"))
-    while True:
-            try:
-                length = float(format((length), ".2f"))
-                break
-            except ValueError:
-                print("Incorrect value was entered. Please try again.")
-                continue
-    
-    width = float(input("Please insert the width of the rectangle:"))
-    while True:
+    def getPerimeter(self,length,width): 
+        self.perimeter = 2*(length+width)
+        self.perimeter = float(format((self.perimeter), ".2f"))
+
+    def getArea(self,length,width):
+        self.area = length*width
+        self.area = float(format((self.area), ".2f"))
+
+    def showResults(self):
+        print("The rectangle perimeter is:", self.perimeter)
+        print("The rectangle area is:", self.area)
+
+length = 0
+width = 0
+while True:
         try:
-            width = float(format((width), ".2f"))
+            length = float(input("Please insert the length of the rectangle:"))
+            length = float(format((length), ".2f"))
             break
         except ValueError:
             print("Incorrect value was entered. Please try again.")
             continue
     
-    perimeter = 2*(length+width)
-    perimeter = float(format((perimeter), ".2f"))
+while True:
+        try:
+            width = float(input("Please insert the width of the rectangle:"))
+            width = float(format((width), ".2f"))
+            break
+        except ValueError:
+            print("Incorrect value was entered. Please try again.")
+            continue
 
-def 
+rectangle = Rectangle()
+rectangle.getPerimeter(length,width)
+rectangle.getArea(length,width)
+rectangle.showResults()
